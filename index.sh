a4033664a682f83664dd8bd9dfaae504e17641af
nakgamers <39780289+nakgamers@users.noreply.github.com>
2019-04-14 05:44:58 +0700

    Update index.sh

--- index.sh
+++ index.sh
@@ -5,22 +5,22 @@ echo "  / |/ /__  / /__ ___    / __/ /____  _______ "
 echo " /    / _ \/ / -_) _ \  _\ \/ __/ _ \/ __/ -_)"
 echo "/_/|_/\___/_/\__/ .__/ /___/\__/\___/_/  \__/ "
 echo "               /_/                            "
-echo "Script Auto Setup FFT(masokky) By Nandar SGB   "
+echo "  Script Auto Setup FFT(masokky) By NS Store  "
 echo "----------------------------------------------"
-echo "Pilih Tools FFT Yang kamu gunakan"
+echo "Pilih Tools Yang kamu gunakan"
 echo "1.VPS"
 echo "2.Termux"
 echo "Nomer Tools:"
 read tools
 
 case $tools in
-	1 )
-		bash vps.sh
-		;;
-	2 )
-		sh termux.sh
-		;;
-	*)
-		echo "Input Yang benar bos"
-		;;
+        1 )
+                bash vps.sh
+                ;;
+        2 )
+                bash termux.sh
+                ;;
+        *)
+                echo "tidak dikenali"
+                ;;
 esac
